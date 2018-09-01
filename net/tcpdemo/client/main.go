d409ff5331322b84faa32519e47ef96ba627811e
zhouteng <wwwsousoucom@126.com>
2018-09-01 22:42:55 +0800

    ok

--- net/tcpdemo/client/main.go
+++ net/tcpdemo/client/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -15,14 +16,24 @@ func main() {
 	}
 	reader := bufio.NewReader(os.Stdin) // os.Stdin 代表标准输入【终端】
 	// 从终端读取一行用户输入，并准备发送给服务器
-	line, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println("readString err=", err)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("readString err=", err)
+			break
+		}
+		// 如果用户输入的是exit就退出
+		line = strings.Trim(line, "\r\n")
+		fmt.Println(line == "exit")
+		if line == "exit" {
+			fmt.Println("客户端退出...")
+			break
+		}
+		// 再将line发送给 服务器
+		n, err := conn.Write([]byte(line))
+		if err != nil {
+			fmt.Println("conn write", err)
+		}
+		fmt.Printf("客户端发送了 %d 字节的数据\n", n)
 	}
-	// 再将line发送给 服务器
-	n, err := conn.Write([]byte(line))
-	if err != nil {
-		fmt.Println("conn write", err)
-	}
-	fmt.Printf("客户端发送了 %d 字节的数据，并退出", n)
 }
