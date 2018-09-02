da346a28242c8107f74af3c74163b8effa61dc56
zhouteng <wwwsousoucom@126.com>
2018-09-02 20:10:21 +0800

    完成

---
+++ net/chatroom/common/message/message.go
@@ -0,0 +1,25 @@
+package message
+
+// 确定一些消息类型
+
+const (
+	LoginMesType    = "LoginMes"
+	LoginResMesType = "LoginResMes"
+)
+
+type Message struct {
+	Type string `json:"type"` // 消息的类型
+	Data string `json:"data"` // 消息的类型
+}
+
+// 定义两个消息...后面需要再增加
+type LoginMes struct {
+	UserId   int    `json:"userId"`   // 用户的id
+	UserPwd  string `json:"userPwd"`  // 用户的密码
+	UserName string `json:"userName"` // 用户名
+}
+
+type LoginResMes struct {
+	Code  int    `json:"code"`  // 返回状态码 500 表示该用户未注册 200 表示登陆成功
+	Error string `json:"error"` // 返回错误信息
+}
