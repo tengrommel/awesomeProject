69fc20d2c711aca314a3ec158f634d0e1971f958
zhouteng <wwwsousoucom@126.com>
2018-09-26 22:18:29 +0800

    工作量证明

---
+++ publicChain/part5-Basic-Prototype/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"awesomeProject/publicChain/part5-Basic-Prototype/BLC"
+	"fmt"
+)
+
+func main() {
+	// 创世区块
+	blockchain := BLC.CreateBlockchainWithGenesisBlock()
+	fmt.Println(blockchain.Blocks[0])
+	// 新区块
+	blockchain.AddBlockToBlockchain("Send 100RMB To zhangqiang",
+		blockchain.Blocks[len(blockchain.Blocks)-1].Height+1,
+		blockchain.Blocks[len(blockchain.Blocks)-1].Hash)
+	blockchain.AddBlockToBlockchain("Send 200RMB To zhouteng",
+		blockchain.Blocks[len(blockchain.Blocks)-1].Height+1,
+		blockchain.Blocks[len(blockchain.Blocks)-1].Hash)
+	blockchain.AddBlockToBlockchain("Send 300RMB To teng",
+		blockchain.Blocks[len(blockchain.Blocks)-1].Height+1,
+		blockchain.Blocks[len(blockchain.Blocks)-1].Hash)
+	blockchain.AddBlockToBlockchain("Send 50RMB To ten",
+		blockchain.Blocks[len(blockchain.Blocks)-1].Height+1,
+		blockchain.Blocks[len(blockchain.Blocks)-1].Hash)
+	fmt.Println(blockchain)
+	fmt.Println(blockchain.Blocks)
+}
