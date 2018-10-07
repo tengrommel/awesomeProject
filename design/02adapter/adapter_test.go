5559114cb2f8fecf073cd704e92f6661a5ce9945
zhouteng <wwwsousoucom@126.com>
2018-10-08 00:13:17 +0800

    adapter

---
+++ design/02adapter/adapter_test.go
@@ -0,0 +1,14 @@
+package adapter
+
+import "testing"
+
+var expect = "adaptee method"
+
+func TestNewAdapter(t *testing.T) {
+	adaptee := NewAdaptee()
+	target := NewAdapter(adaptee)
+	res := target.Request()
+	if res != expect {
+		t.Fatalf("expect: %s, actual: %s", expect, res)
+	}
+}
