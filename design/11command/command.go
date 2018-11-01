9c875705c25b9b335833a668a5f81ac9bbd143db
zhouteng <wwwsousoucom@126.com>
2018-11-01 18:19:25 +0800

    command

---
+++ design/11command/command.go
@@ -0,0 +1,55 @@
+package command
+
+import "fmt"
+
+type Command interface {
+	Execute()
+}
+
+type StartCommand struct {
+	mb *MotherBoard
+}
+
+func NewStartCommand(mb *MotherBoard) *StartCommand {
+	return &StartCommand{
+		mb: mb,
+	}
+}
+
+func (c *StartCommand) Execute() {
+	c.mb.Start()
+}
+
+type RebootCommand struct {
+	mb *MotherBoard
+}
+
+type MotherBoard struct{}
+
+func (*MotherBoard) Start() {
+	fmt.Print("system starting\n")
+}
+
+func (*MotherBoard) Reboot() {
+	fmt.Print("system rebooting\n")
+}
+
+type Box struct {
+	button1 Command
+	button2 Command
+}
+
+func NewBox(button1, button2 Command) *Box {
+	return &Box{
+		button1: button1,
+		button2: button2,
+	}
+}
+
+func (b *Box) PressButton1() {
+	b.button1.Execute()
+}
+
+func (b *Box) PressButton2() {
+	b.button2.Execute()
+}
