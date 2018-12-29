bae1d3b7d9bc51e0dcd44d91671140ca9f684cd1
zhouteng <wwwsousoucom@126.com>
2018-12-29 15:17:42 +0800

    singleton

---
+++ oop/designPatterns/chapter01/libraries/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"awesomeProject/oop/designPatterns/chapter01/libraries/arithmetic"
+	"fmt"
+)
+
+func main() {
+	sumRes := arithmetic.Sum(5, 6)
+	subRes := arithmetic.Subtract(10, 5)
+	multiplyRes := arithmetic.Multiply(8, 7)
+	divideRes, err := arithmetic.Divide(10, 2)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("5+6 is %d. 10-5 is %d, 8*7 is %d and 10/2 is %f\n", sumRes, subRes, multiplyRes, divideRes)
+}
