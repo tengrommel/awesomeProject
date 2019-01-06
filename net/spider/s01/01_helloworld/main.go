9234f3aff6ed14cbd809c249cdc8235dc15a733d
zhouteng <wwwsousoucom@126.com>
2019-01-06 13:05:27 +0800

    ok

---
+++ net/spider/s01/01_helloworld/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+	concepts()
+}
+
+func concepts() {
+	firstName := "John"
+	firstName = "Nick"
+	fmt.Println(firstName)
+	fmt.Println(4 / 2 * 3)
+	var i = 20
+	i++
+}
---
+++ net/spider/s01/02_types_variables/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	fmt.Println("i =", i)
+	j := 2
+	fmt.Printf("%d - %T\n", j, j)
+	var k1 uint8 = 20
+	fmt.Printf("%d - %T\n", k1, k1)
+	var k2 uint16 = 30
+	k2 = uint16(k1)
+	fmt.Printf("%d - %T\n", k2, k2)
+	fmt.Println(uint64(321325 * 424521))
+}
