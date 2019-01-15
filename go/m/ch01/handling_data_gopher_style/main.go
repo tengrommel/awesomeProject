e99100372f9ce1c34445631709c4104aad9545f0
zhouteng <wwwsousoucom@126.com>
2019-01-15 12:11:30 +0800

    csv

---
+++ go/m/ch01/handling_data_gopher_style/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	f, err := os.Open("./myfile.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Read in the CSV records.
+	r := csv.NewReader(f)
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var intMax int
+	for _, record := range records {
+		intVal, err := strconv.Atoi(record[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		if intVal > intMax {
+			intMax = intVal
+		}
+	}
+	fmt.Println(intMax)
+}
