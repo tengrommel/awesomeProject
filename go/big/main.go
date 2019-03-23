4673cf9bcf06eae5c9c27f5d2ff3b99c09f29a32
tengrommel <wwwsousoucom@126.com>
2019-03-23 20:32:12 +0800

    big

--- go/big/main.go
+++ go/big/main.go
@@ -1,24 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/axgle/mahonia"
-	"io/ioutil"
-	"strings"
+	"io"
+	"os"
 )
 
 func main() {
-	contentBytes, err := ioutil.ReadFile("kai.txt")
-	if err != nil {
-		fmt.Println("读入失败", err)
-	}
-	contentStr := string(contentBytes)
-	// 逐行打印
-	lineStrs := strings.Split(contentStr, "\n")
-	for _, lineStr := range lineStrs {
-		fmt.Println(lineStr)
-		newStr, _ := convertEncoding(lineStr, "GBK", "UTF-8")
-		fmt.Println(newStr)
+	//contentBytes, err := ioutil.ReadFile("kai.txt")
+	//if err != nil {
+	//	fmt.Println("读入失败", err)
+	//}
+	//contentStr := string(contentBytes)
+	//// 逐行打印
+	//lineStrs := strings.Split(contentStr, "\n")
+	//for _, lineStr := range lineStrs {
+	//	fmt.Println(lineStr)
+	//	newStr, _ := convertEncoding(lineStr, "GBK", "UTF-8")
+	//	fmt.Println(newStr)
+	//}
+	file, _ := os.Open("file.txt")
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	for {
+		lineBytes, _, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		gbkStr := string(lineBytes)
+		utfStr, _ := convertEncoding(gbkStr, "GBK", "UTF-8")
+		fmt.Println(utfStr)
 	}
 }
 
