d166eb66c568d2d8457e712d0155cabeba851150
zhouteng <wwwsousoucom@126.com>
2019-04-27 01:09:55 +0800

    ok

--- algorithm/linked_list/ex2/main.go
+++ algorithm/linked_list/ex2/main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
-	"crypto/hmac"
-	"crypto/sha1"
 	"fmt"
+	"github.com/isdamir/gotype"
 )
 
+/*
+	如何从无序链表中移除重复项
+	给定一个没有排序的链表，去掉其重复项，并保留原顺序
+*/
+
+//  顺序删除
+func RemoveDup(head *gotype.LNode) {
+	if head == nil || head.Next == nil {
+		return
+	}
+	outerCur := head.Next
+	var innerCur *gotype.LNode
+	var innerPre *gotype.LNode
+	for ; outerCur != nil; outerCur = outerCur.Next {
+		for innerCur, innerPre = outerCur.Next, outerCur; innerCur != nil; {
+			if outerCur.Data == innerCur.Data {
+				innerPre.Next = innerCur.Next
+				innerCur = innerCur.Next
+			} else {
+				innerPre = innerCur
+				innerCur = innerCur.Next
+			}
+		}
+	}
+}
+
 func main() {
-	//h := hmac.New(sha1.New, key)
-	//h.Write(bytes)
-	//return fmt.Sprintf(hex.EncodeToString(h.Sum(nil)))
-	key := []byte("123456")
-	h := hmac.New(sha1.New, key)
-	h.Write([]byte("aaaaaa"))
-	fmt.Printf("%x\n", h.Sum(nil))
+	fmt.Println("删除重复结点")
 }
